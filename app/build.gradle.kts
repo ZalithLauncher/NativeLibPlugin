308253a9229eccb0ed64eab9ac465389df5e7ecc
movte <movtery228@qq.com>
2026-05-29 18:32:27 +0800

    更新lib：e57d626

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         }
     }
 
-    val versionString = "0.1.0_9c6b6d1 NDK BUILD"
+    val versionString = "e57d626 NDK BUILD"
 
     defaultConfig {
         applicationId = "com.commonlauncher.nativeplugin"
